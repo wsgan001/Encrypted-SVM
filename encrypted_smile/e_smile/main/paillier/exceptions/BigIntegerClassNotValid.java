89dcfd706fab08aee4fd94ca8c72f899fcb3e159
Kevin Huang <khuan042@uottawa.ca>
2017-11-30 06:34:01 -0500

    added paillier files

---
+++ encrypted_smile/e_smile/main/paillier/exceptions/BigIntegerClassNotValid.java
@@ -0,0 +1,13 @@
+package paillier.exceptions;
+
+public class BigIntegerClassNotValid extends Exception {
+
+	/**
+	 * Default Serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BigIntegerClassNotValid(String msg) {
+		super(msg);
+	}
+}
---
+++ encrypted_smile/e_smile/main/paillier/exceptions/SizesNotEqualException.java
@@ -0,0 +1,13 @@
+package paillier.exceptions;
+
+public class SizesNotEqualException extends Exception {
+
+	/**
+	 * Default serial UID
+	 */
+	private static final long serialVersionUID = 1L;
+	public SizesNotEqualException(String msg) {
+		super(msg);
+	}
+
+}
